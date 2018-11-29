f0e3f16e9a32341179a456e3e170575754fa3334
Otávio Murilo <murilotavio10@gmail.com>
2018-11-29 11:30:59 -0200

    Consertando bug no JSON

--- jsonFormat/henrique.py
+++ jsonFormat/henrique.py
@@ -1,10 +1,4 @@
-# -*- coding: latin1 -*-
-####################################
 import json, os, shutil
-
-####################################
-# Status - Versão 1 - Ler arquivos JSON que foram coletados
-####################################
 
 ####################################
 # Leitura dos arquivos
@@ -16,21 +10,25 @@ def read_file(file):
             tweets = f.readlines()                            # Leitura do arquivo atual
             for line in tweets:                               # Para cada linha do arquivo atual
                 line = line.split('\\r\\n\"')                 # Separa os tweets de acordo com o Carriage Return e EOF
-                for item in line:                             # Último item � ' '. Tem que tratar...
+                for item in line:                             # Último item ' '. Tem que tratar...
                     if item != '':                            # Tratando final do arquivo aqui!
                         item = item.replace('\"{\\\"created','{\\\"created')
                         item = item.replace('\\\"','\"')
                         item = item.replace('\\\\','\\')
-                        item = json.loads(item)               # Converte a string em dicionario JSON para testar se está OK.
-                        g.write(json.dumps(item) + "\n")      # Salva o dicionario no novo arquivo.
+                        try:
+                            item = json.loads(item)               # Converte a string em dicionario JSON para testar se está OK.
+                            g.write(json.dumps(item) + "\n")      # Salva o dicionario no novo arquivo.
+                            print("OK")
+                        except:
+                            print("Erro no json.dumps()")
                     else:
                         print("Fim do Arquivo!")
-####################################
-# Metodo principal do programa
-####################################
+                        
+########################### main
+
 def main():
-    os.system('clear')
-    print ("\n#######################################################################\n")
+    os.system('cls')
+    print ("\n####################################################################### inicio\n")
     i = 0
     for file in os.listdir(data_dir):                          # Para cada arquivo no diretorio do candidato
         if not os.path.isfile(data_dir+file):
@@ -40,11 +38,11 @@ def main():
             try:
                 read_file(file)                                    # Chama a funcao para criar o novo arquivo JSON tratado
             except:
+                print("ERROOOO")
                 shutil.copy(data_dir+file, output_dir_erro)
             i+=1
     print("Total de arquivos tratados: "+str(i))
 
-    print
     print("######################################################################")
     print("Script finalizado!")
     print("######################################################################\n")
@@ -54,9 +52,9 @@ def main():
 ####################################
 
 candidato = "henrique"
-data_dir = "../data/"+candidato+"/"          # Substituir "eleicao" pelo nome do diretorio do candidato
-output_dir = "../data_new/"+candidato+"_new/"    # Substituir "eleicao" conforme linha acima e manter o "_new" para criar um novo diretorio
-output_dir_erro = "../data_erro/"+candidato+"_erro"
+data_dir = "./data/"+candidato+"/"          # Substituir "eleicao" pelo nome do diretorio do candidato
+output_dir = "./data_new/"+candidato+"_new/"    # Substituir "eleicao" conforme linha acima e manter o "_new" para criar um novo diretorio
+output_dir_erro = "./data_erro/"+candidato+"_erro"
 
 #Cria os diretorios para armazenamento dos novos arquivos
 if not os.path.exists(output_dir):
