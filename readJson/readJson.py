ecaa77b05333338b35f6e3d275f4247cb1fc07fe
Otávio Murilo <murilotavio10@gmail.com>
2018-12-11 16:44:02 -0200

    Organizando

--- readJson/readJson.py
+++ readJson/readJson.py
@@ -2,8 +2,4 @@ import pyorient
 
 client = pyorient.OrientDB("localhost", 2480)
 session_id = client.connect("root","Pivic2018")
-client.db_open("demodb", "root", "Pivic2018")
-
-print(client.command("select from Castles"))
-
-client.shutdown("root", "Pivic2018")
+client.db_open("eleicoesbrasil2018", "root", "Pivic2018")
